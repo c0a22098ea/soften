f1069ba8eac6afc4aad00b57183433c7f672b700
Shusuke Chida <c0a22098ea@edu.teu.ac.jp>
2023-12-10 23:15:10 +0900

    step4

--- PurchasePointment.java
+++ PurchasePointment.java
@@ -4,15 +4,15 @@ import java.time.LocalDateTime;
     final ComicId comicId;
     final PurchasePoint consumptionPoint;
     final LocalDateTime paymentDateTime;
+
     PurchasePointPayment(final Customer customer, final Comic comic) {
-        if (!customer.isEnabled()) {
+        if (customer.isEnabled()) {
             throw new IllegalArgumentException("有効な購入者ではありません。");
         }
-	if (!comic.isEnabled()) {
+	if (comic.isEnabled()) {
 	    throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
-	}
-	if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
-	    throw new RuntimeException("所持ポイントが不足しています。");
+	}    if (customer.possessionPoint.amount < comic.currentPurchasePoint.amount) {
+	  throw new RuntimeException("所持ポイントが不足しています。");
 	}
 	customerId = customer.id;
 	comicId = comic.id;
